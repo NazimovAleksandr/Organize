9b5d46643e9af944e08c0e6f72d738473694b6dd
Назимов Александр <50_Sanya@mail.ru>
2023-08-04 15:57:30 +0500

    Core: Theme

---
+++ core/src/main/java/com/core/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.core.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val SecondaryWhite = Color(0xFFFFFFFF)
+val PrimaryText = Color(0xFF262736)
+val PriorityNotAssigned = Color(0xFF7E7E79)
